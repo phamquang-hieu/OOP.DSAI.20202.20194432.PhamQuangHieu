bdfa64d98d0d320fbfae52e11f5793dde31f0f18
phamquang-hieu <phamhieu1906@gmail.com>
2021-04-07 20:20:27 +0700

    submit topic/store and fix a small bugs on id in DigitalVideoDisc

---
+++ AimsProject/src/StoreTest.java
@@ -0,0 +1,17 @@
+
+public class StoreTest {
+	public static void main(String args[]) {
+		Store x = new Store();
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Stars Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Alladin", "Animation", 18.99f);
+		DigitalVideoDisc dvd4 = new DigitalVideoDisc("abc");
+		DigitalVideoDisc dvdList[] = {dvd1, dvd2, dvd3, dvd4};
+		x.addDVD(dvdList);
+		x.removeDVD(dvd4);
+		x.removeDVD(dvd3);
+		x.removeDVD(dvd2);
+		x.removeDVD(dvd1);
+		x.removeDVD(new DigitalVideoDisc("xyz"));
+	}
+}
