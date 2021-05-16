33fbd501ccf7ccbc6d1140adf041e869f3cadb8d
phamquang-hieu <phamhieu1906@gmail.com>
2021-05-16 21:05:01 +0700

    submition for Play and Remove  function in Cartscreen:

---
+++ AimsProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -0,0 +1,99 @@
+package hust.soict.dsai.aims.screen;
+
+import javax.swing.JOptionPane;
+
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.media.CompactDisc;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
+import hust.soict.dsai.aims.media.Playable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class CartScreenController {
+	
+	private Cart cart;
+	
+	@FXML
+	private TableView<Media> tblMedia;
+	
+	@FXML
+	private TableColumn<Media, String> colMediaTitle;
+	
+	@FXML
+	private TableColumn<Media, String> colMediaCategory;
+	
+	@FXML
+	private TableColumn<Media, Float> colMediaCost;
+	
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnRemove;
+	
+	public CartScreenController(Cart cart) {
+		super();
+		this.cart = cart;
+	}
+	
+	@FXML
+	private void initialize() {
+		colMediaTitle.setCellValueFactory(
+				new PropertyValueFactory<Media, String>("Title"));
+		colMediaCategory.setCellValueFactory(
+				new PropertyValueFactory<Media, String>("Category"));
+		colMediaCost.setCellValueFactory(
+				new PropertyValueFactory<Media, Float>("Cost"));
+		tblMedia.setItems(this.cart.getItemsOrdered());
+		
+		btnPlay.setVisible(false);
+		btnRemove.setVisible(false);
+		
+		tblMedia.getSelectionModel().selectedItemProperty().addListener(
+				new ChangeListener<Media>() {
+					@Override
+					public void changed(ObservableValue<? extends Media> observable, Media oldValue, Media newValue) 
+					{
+						if(newValue != null) {
+								updateButtonBar(newValue);
+							}
+					}
+				});
+	}
+	void updateButtonBar(Media media) {
+		btnRemove.setVisible(true);
+		if(media instanceof Playable) {
+			btnPlay.setVisible(true);
+		} else {
+			btnPlay.setVisible(false);
+		}
+	}
+	
+	@FXML
+	void btnRemovePressed(ActionEvent event) {
+		Media media = tblMedia.getSelectionModel().getSelectedItem();
+		cart.removeMedia(media);
+	}
+	
+	@FXML
+	void playButtonPressed(ActionEvent event) {
+		Media media = tblMedia.getSelectionModel().getSelectedItem();
+		if(media instanceof DigitalVideoDisc) {
+			DigitalVideoDisc d = (DigitalVideoDisc) media;
+			JOptionPane.showMessageDialog(null, d.play());
+		}
+		else if(media instanceof CompactDisc) {
+			CompactDisc c = (CompactDisc) media;
+			JOptionPane.showMessageDialog(null, c.play());
+		}
+	}
+	
+
+}
